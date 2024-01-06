3adc1f6c2e890276e6eff899c8d4ca853041d293
AlexVorobiev001 <wav.tsk@yandex.ru>
2024-01-06 22:31:39 +0700

    SkillFactoryProject 27.8

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,9 +3,13 @@ import comparator.UniversityComparator;
 import enums.StudentComparatorType;
 import enums.UniversityComparatorType;
 import io.XlsReader;
+import io.XlsWriter;
+import model.Statistics;
 import model.Student;
 import model.University;
 import util.ComparatorUtil;
+import util.JsonUtil;
+import util.StatisticsUtil;
 
 import java.io.IOException;
 import java.util.List;
@@ -18,16 +22,43 @@ public class MainClass {
                 XlsReader.readXlsUniversities("src/main/resources/universityInfo.xlsx");
         UniversityComparator universityComparator =
                 ComparatorUtil.getUniversityComparator(UniversityComparatorType.YEAR);
-        universities.stream()
-                .sorted(universityComparator)
-                .forEach(System.out::println);
+        universities.sort(universityComparator);
+        String universitiesJson = JsonUtil.universityListToJson(universities);
+        // проверяем, что json создан успешно
+        System.out.println(universitiesJson);
+        List<University> universitiesFromJson = JsonUtil.jsonToUniversityList(universitiesJson);
+        // проверяем, что обратно коллекция воссоздаётся в таком же количестве элементов
+        System.out.println(universities.size() == universitiesFromJson.size());
+        universities.forEach(university -> {
+            String universityJson = JsonUtil.universityToJson(university);
+            // проверяем, что json из отдельного элемента создан успешно
+            System.out.println(universityJson);
+            University universityFromJson = JsonUtil.jsonToUniversity(universityJson);
+            // проверяем, что обратно элемент воссоздаётся
+            System.out.println(universityFromJson);
+        });
 
         List<Student> students =
                 XlsReader.readXlsStudents("src/main/resources/universityInfo.xlsx");
         StudentComparator studentComparator =
                 ComparatorUtil.getStudentComparator(StudentComparatorType.AVG_EXAM_SCORE);
-        students.stream()
-                .sorted(studentComparator)
-                .forEach(System.out::println);
+        students.sort(studentComparator);
+        String studentsJson = JsonUtil.studentListToJson(students);
+        // проверяем, что json создан успешно
+        System.out.println(studentsJson);
+        List<Student> studentsFromJson = JsonUtil.jsonToStudentList(studentsJson);
+        // проверяем, что обратно коллекция воссоздаётся в таком же количестве элементов
+        System.out.println(students.size() == studentsFromJson.size());
+        students.forEach(student -> {
+            String studentJson = JsonUtil.studentToJson(student);
+            // проверяем, что json из отдельного элемента создан успешно
+            System.out.println(studentJson);
+            Student studentFromJson = JsonUtil.jsonToStudent(studentJson);
+            // проверяем, что обратно элемент воссоздаётся
+            System.out.println(studentFromJson);
+        });
+
+        List<Statistics> statisticsList = StatisticsUtil.createStatistics(students, universities);
+        XlsWriter.writeXlsStatistics(statisticsList, "statistics.xlsx");
     }
 }
